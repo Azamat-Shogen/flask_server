1f6ed3ba0278d59372ce9bbe4cb5413e8d497a60
Azamat Shogen <baki47hanma@gmail.com>
2021-11-21 14:01:01 -0600

    actors endpoint work

--- tube_tv/src/api/actors.py
+++ tube_tv/src/api/actors.py
@@ -41,3 +41,51 @@ def add_actor():
         return jsonify(actor.serialize())
     except:
         return custom_error("Failed to add")
+
+
+@bp.route('/<int:actor_id>', methods=['GET'])
+def get_actor(actor_id: int):
+    try:
+        actor = Actor.query.get_or_404(actor_id)
+        return jsonify(actor.serialize())
+    except:
+        return custom_error("Something went wrong", 404)
+
+
+@bp.route('/<int:actor_id>', methods=['DELETE'])
+def delete_actor(actor_id: int):
+    try:
+        actor = Actor.query.get_or_404(actor_id)
+        db.session.delete(actor)
+        db.session.commit()
+        return jsonify({"message": "actor deleted successfully"})
+    except:
+        return custom_error("Failed to delete!", 404)
+
+
+@bp.route('/<int:actor_id>', methods=['PUT', 'PATCH'])
+def update_actor(actor_id: int):
+    actors = Actor.query.all()
+    actor = Actor.query.get_or_404(actor_id)
+
+    filtered = list(filter
+                    (lambda x: x.first_name.lower() == request.json['first_name'].lower()
+                               and x.last_name.lower() == request.json['last_name'].lower()
+                               and x.id != actor.id, actors))
+    if len(filtered) > 0:
+        return custom_error("Actor already exists", 400)
+
+    try:
+        if 'first_name' in request.json:
+            actor.first_name = request.json['first_name']
+        if 'last_name' in request.json:
+            actor.last_name = request.json['last_name']
+
+        db.session.commit()
+        return jsonify({"message": "actor updated successfully"})
+    except:
+        return custom_error("Something went wrong", 404)
+
+
+
+
