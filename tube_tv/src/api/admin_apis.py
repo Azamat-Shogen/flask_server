3ce16a1b61f593cfa1d9879633da4e436d6880b3
Azamat Shogen <baki47hanma@gmail.com>
2021-11-23 00:29:00 -0600

    film_genre relat. complete

--- tube_tv/src/api/admin_apis.py
+++ tube_tv/src/api/admin_apis.py
@@ -1,6 +1,6 @@
 import sqlalchemy
-from flask import Blueprint, json, jsonify, abort, request, make_response
-from ..models import User, Film, Genre, db, film_actors, Actor
+from flask import jsonify, request, make_response
+from ..models import Film, Genre, db, film_actors, film_genres
 from flask import Blueprint
 
 bp = Blueprint('admin', __name__, url_prefix='/admin')
@@ -45,4 +45,45 @@ def delete_film_actor(film_id: int):
         return jsonify({"message": "actor removed from film"})
     except:
         return custom_error("something went wrong", 422)
+
+
+# TODO: POST
+@bp.route('film-setup-genre/<int:film_id>', methods=['POST'])
+def add_genre_to_film(film_id: int):
+    if "genre_id" not in request.json:
+        return custom_error("genre_id is required", 400)
+
+    genre_id = request.json['genre_id']
+    film = Film.query.get_or_404(film_id)
+    genre = Genre.query.get_or_404(genre_id)
+
+    try:
+        stmt = sqlalchemy.insert(film_genres).values(film_id=film_id, genre_id=genre_id)
+        db.session.execute(stmt)
+        db.session.commit()
+        return jsonify(True)
+    except:
+        return custom_error("Something went wrong", 422)
+
+
+# TODO: DELETE
+@bp.route('film-setup-genre/<int:film_id>', methods=['DELETE'])
+def delete_film_genre(film_id: int):
+    if "genre_id" not in request.json:
+        return custom_error("genre_id is required", 400)
+
+    genre_id = request.json['genre_id']
+    film = Film.query.get_or_404(film_id)
+    genre = Genre.query.get_or_404(genre_id)
+
+    try:
+        stmt = sqlalchemy.delete(film_genres).where(film_genres.c.film_id == film_id) \
+               .where(film_genres.c.genre_id == genre_id)
+
+        db.session.execute(stmt)
+        db.session.commit()
+        return jsonify({"message": "genre deleted from film"})
+    except:
+        return custom_error("Something went wrong", 422)
+
 
